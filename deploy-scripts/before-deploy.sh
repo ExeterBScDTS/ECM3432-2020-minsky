99392309dc68cb4f8258b82fa31407d62e53b37e
Michael Saunby <mike@saunby.net>
2020-04-09 00:19:02 +0100

    .

--- deploy-scripts/before-deploy.sh
+++ deploy-scripts/before-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 (cd minsky-one; tar czvf /home/travis/install-${TAGNAME}.tar.gz install)
-(cd minsky-one; zip /home/travis/minskyOne-${TAGNAME}.zip target/minksyOne-0.2.war)
+(cd minsky-one; zip /home/travis/minskyOne-${TAGNAME}.zip target/minskyOne-0.2.war)
